3b5ee96df31ace840feaf2ed588785949aba246f
BlackTeemo <80164170@qq.com>
2021-04-10 23:45:05 +0800

    深浅拷贝.py

--- 深浅拷贝.py
+++ 深浅拷贝.py
@@ -1,6 +1,6 @@
 # -*- coding = utf-8 -*-
 # @Time : 2021/4/2 1:32
-# @Author : 郭庭宇
+# @Author : BlackTeemo
 # @File : 深浅拷贝.py
 # @Software: PyCharm
 def MyDeepcopy(list):
@@ -22,50 +22,6 @@ def MyDeepcopy(list):
 
     return CopyList
 
-    # Tmp = []
-    # for i in Element:
-    #     Tmp.append(i)
-    # CopyList.append(Tmp)
-
-
-if __name__ == "__main__":
-    list1 = [1, [11, 22], {"a": 3}]
-    # ret = '{}'.format(type(list1))
-    # print(ret)
-
-    #print(str(type(list1)) == "<class 'list'>")
-    '''
-    #  浅拷贝
-    list2 = list1.copy()
-    print(id(list1),id(list2))
-    list1[0] = 2
-    print(list1[0],list2[0])
-    list1[1][0] = 22
-    list1[1][1] = 33
-    print(list1[1],list2[1])
-    '''
-
-    #深拷贝
-    # list2 = MyDeepcopy(list1)
-    # # print(id(list1), id(list2))
-    # list1[0] = 2
-    # print(list1[0], list2[0])
-    #
-    # list1[1][0] = 22
-    # list1[1][1] = 33
-    # print(list1[1], list2[1])
-    #
-    # list1[2]["a"] = 4
-    # print(list1[2], list2[2])
-    #
-    # print(list1,list2)
-
-    direc = {"123": 2, "234": 3}
-    direc2 = MyDeepcopy(direc)
-
-    direc['123'] = 3
-    print(direc)
-    print(direc2)
 
 
 
